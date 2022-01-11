1b19705c1320311320d4e176e6bb238b10aad49f
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-01-11 23:40:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.00%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -3,10 +3,10 @@ public:
     int bitwiseComplement(int n) {
         if(n==0)
             return 1;
-        int cp=n;
+        int boi=n;
         int i=0;
-        while(cp!=0){
-            cp>>=1;
+        while(boi!=0){
+            boi>>=1;
             n^=(1<<i);
             i++;
         }
