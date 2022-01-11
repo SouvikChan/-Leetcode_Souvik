df0b41c8d353db1bda25f5300b8fff4007b43640
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-01-11 23:48:06 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (38.23%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int n=s.size();
+        int i=0;
+        int res=0;
+        while(i<n){
+            if(s[i]=='X'){
+                res++;
+                i=i+3;
+            }
+            else{
+                i++;
+            }
+        }
+        return res;
+    }
+};
