edcf1c4698a63fb6131ad0f90087cd4ceff40805
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-01-16 19:41:11 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (77.63%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue pq(stones.begin(),stones.end());
+        while(pq.size()!=1){
+            int a=pq.top(); pq.pop();
+            int b=pq.top(); pq.pop();
+            pq.push(abs(a-b));
+        }
+        return pq.top();
+    }
+};
