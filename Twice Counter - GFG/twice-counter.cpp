7ec7ce60ce363bc74641eb5263aaf7fe43ff8a40
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-01-25 21:57:55 +0530

    Added solution - LeetHub

---
+++ Twice Counter - GFG/twice-counter.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+        int countWords(string list[], int n)
+        {
+           //code here.
+          map<string, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            mp[list[i]]++;
+        }
+        int cnt = 0;
+        for (auto it : mp)
+        {
+            if (it.second == 2)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+        }
+
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string list[n];
+        for(int i=0;i<n;i++)
+            cin>>list[i];
+        Solution ob;    
+        cout <<ob.countWords(list, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
