0ec6b954a272b27d33210daccad55c133c173b6c
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-02-06 19:21:29 +0530

    Time: 17 ms (45.94%), Space: 8 MB (94.71%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string ans;
+        if(numRows==1) return s;
+        int step= 2*numRows-2;
+        for(int i=0;i<numRows;i++){
+            if(i==0 || i==numRows-1)
+                for(int j=i;j<s.size();j+=step){
+                    ans.push_back(s[j]);
+                }
+            else{
+                int j=i;
+                bool flag=true;
+                int step1=2*(numRows-1-i);
+                int step2=step-step1;
+                while(j<s.length()){
+                    ans.push_back(s[j]);
+                    if(flag) j+=step1;
+                    else j+=step2;
+                    flag =!flag;
+                }
+            }
+        }
+        return ans;
+    }
+};
