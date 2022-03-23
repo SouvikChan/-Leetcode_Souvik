0453518492bc94f940185fa8c038c6958182b877
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-03-23 23:48:07 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (73.18%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int cnt=0;
+        while(target>startValue){
+            cnt++;
+            if(target%2==0) target/=2;
+            else{
+                target+=1;
+            }
+        }
+        cnt+=(startValue-target);
+        return cnt;
+    }
+};
