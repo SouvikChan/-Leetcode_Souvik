f1946ca4eae8abfdc5859ed6151a287a126a1f37
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-04-06 23:49:27 +0530

    Time: 134 ms (27.36%), Space: 28.2 MB (73.69%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        map<int,int> mp;
+        for(int i=0;i<hand.size();++i){
+            mp[hand[i]]++;
+        }
+        while(mp.size()>0){
+            int minVal=mp.begin()->first;
+            for(int i=minVal;i<minVal+groupSize;i++){
+                if(mp.find(i)==mp.end())
+                    return false;
+                int op = mp.find(i)->second;
+                if(op == 1)
+                    mp.erase(i);
+                else
+                    mp[i] = op-1;
+            }
+        }
+        return true;
+    }
+};
