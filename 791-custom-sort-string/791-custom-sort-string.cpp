e404817b27dddbc73db151ef32d57b87f682e842
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-04-07 11:15:33 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (95.87%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.cpp
@@ -0,0 +1,16 @@
+
+    string str;
+    bool comparetor(char c1, char c2){
+        return str.find(c1)<str.find(c2);
+    }
+
+class Solution {
+public:
+   
+    
+    string customSortString(string order, string s) {
+        str=order;
+        sort(s.begin(),s.end(),comparetor);
+        return s;
+    }
+};
