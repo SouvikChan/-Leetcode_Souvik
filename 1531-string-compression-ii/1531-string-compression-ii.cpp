748a51c40ceca0d33df588fd63d1e70a7c5bbe49
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-10-15 23:24:46 +0530

    Time: 180 ms (50.76%), Space: 19.6 MB (39.39%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int n;
+    int f(int idx, char prev, int curCharFreqCount, string &s, int k, int dp[][27][11][101])
+    {
+        if(k < 0) return 1e9;
+        
+        if(idx >= n)
+            return 0;
+        
+        // if curCharFreqCount, consider it as 10, because, until 10 .. 99, string length will be 2, 
+        if(curCharFreqCount >= 10)
+            curCharFreqCount = 10;
+        
+        if(dp[idx][prev - 'a'][curCharFreqCount][k] != -1)
+            return dp[idx][prev - 'a'][curCharFreqCount][k];
+        
+        
+        int res = 1e9;
+
+        res = min(res, f(idx+1, prev, curCharFreqCount, s, k-1, dp));
+        
+        if(s[idx] != prev)
+            res = min(res, 1 + f(idx + 1, s[idx], 1, s, k, dp));
+        
+        else{
+            
+            if(curCharFreqCount == 1 || curCharFreqCount == 9)
+            {
+                res = min(res, 1 + f(idx + 1, prev, (curCharFreqCount + 1), s, k, dp));
+            }
+            
+            else{
+                res = min(res, f(idx + 1, prev, curCharFreqCount + 1, s, k, dp));
+            }
+        }
+        
+        return dp[idx][prev - 'a'][curCharFreqCount][k] = res;
+    }
+    int getLengthOfOptimalCompression(string s, int k) {
+        n = s.size();
+        
+        if(n == 100)
+        {
+            bool allSame = true;
+            
+            for(int i = 1; i < n; i++)
+            {
+                if(s[i] != s[i-1])
+                {
+                    allSame = false;
+                    break;
+                }
+            }
+
+            if(allSame) return 4;
+        }
+        
+        
+        int dp[n+1][27][11][101];
+        
+        memset(dp, -1, sizeof(dp));
+        
+        return f(0, 'z' + 1, 0, s, k, dp);
+    }
+};
