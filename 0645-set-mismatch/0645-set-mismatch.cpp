6f1be3326515f2705081062811cf62963b6be26b
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-10-23 16:47:54 +0530

    Time: 75 ms (55.79%), Space: 21.4 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n=size(nums);
         sort(nums.begin(),nums.end());
-        int dup=-1;
-        for(int i=0;i<n-1;++i){
+        int dup=0;
+        for(int i=0;i<n;++i){
             if(nums[i]==nums[i+1]){
                 dup=nums[i];
                 break;
