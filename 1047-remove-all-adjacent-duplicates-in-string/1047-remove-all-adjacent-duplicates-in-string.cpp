cfd687d92f3a79df41d2c466378502065e51bf66
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-11-10 23:18:30 +0530

    Time: 16 ms (95.61%), Space: 11 MB (66.38%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans="";
+        for(auto &i:s){
+            if(ans.size() && i==ans.back()) ans.pop_back();
+            else ans.push_back(i);
+        }
+        return ans;
+    }
+};
