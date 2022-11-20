e1ce11812aa6d8980fb0eafecf5781beb4fb91c6
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-11-20 23:26:05 +0530

    Time: 26 ms (43.62%), Space: 8.2 MB (43.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int ans = 0;
+        int sum=0;
+        int sign=1;
+        stack<int>st;
+        int size=s.size();
+        for(int i=0;i<size;++i){
+            if(isdigit(s[i])){
+                sum=s[i]-'0';
+                while(i+1<size && isdigit(s[i+1])){
+                    sum = sum*10 + (s[i+1] -'0');
+                    i++;
+                }
+                ans+=sum*sign;
+            }
+            else if(s[i] == '+')
+                sign=1;
+            else if (s[i] == '-')
+                sign=-1;
+            else if (s[i] == '('){
+                st.push(ans);
+                st.push(sign);
+                ans=0;
+                sign=1;
+            }
+            else if(s[i] == ')')
+            {
+                int xsign=st.top();
+                st.pop();
+                int xans= st.top();
+                st.pop();
+                ans*=xsign;
+                ans+= xans;
+            }
+        }
+        return ans;
+    }
+};
