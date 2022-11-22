32f4e69345fc51960b3971d4eb2525ff4e150b6e
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-11-22 13:00:03 +0530

    Time: 376 ms (55.98%), Space: 8.9 MB (81.88%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     
-    int helper(int i,vector<int>&dp){
-        if(i==0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int minCnt=INT_MAX;
-        for(int num=1;num<=sqrt(i);++num){
-            int curCnt=1+helper(i-(num*num),dp);
-            minCnt=min(curCnt,minCnt);
-        }
-        return dp[i]=minCnt;
-    }
-    
     int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-        return helper(n,dp);
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int nums=1;nums<=n;++nums){
+            int minCnt = INT_MAX;
+            for (int num = 1; num <= sqrt(nums); num++)
+            {
+                int curCnt = 1 + dp[nums - (num*num)];
+                minCnt = min(minCnt, curCnt);
+            }
+            dp[nums] = minCnt;
+        }
+        return dp[n];
     }
 };
