a3429da7c49a43ac8f79d01c31421e51860d34ae
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-12-16 10:07:21 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (83.10%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,29 @@
+class MyQueue {
+    private Deque<Integer> in_stk = new ArrayDeque<>();
+    private Deque<Integer> out_stk = new ArrayDeque<>();
+    public void push(int x) {
+        in_stk.push(x);
+    }
+    public int pop() {
+        peek();
+        return out_stk.pop();
+    }
+    public int peek() {
+        if (out_stk.isEmpty())
+        while (!in_stk.isEmpty())
+            out_stk.push(in_stk.pop());
+        return out_stk.peek();
+    }
+    public boolean empty() {
+        return in_stk.isEmpty() && out_stk.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
