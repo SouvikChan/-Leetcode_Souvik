9431e260debf2acd9a46a2eae06415968ab61144
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2026-02-08 21:07:16 +0530

    Time: 19 ms (77.31%), Space: 17.6 MB (23.65%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,51 +1,31 @@
-class Solution
-{
-    public:
-        vector<vector < int>> fourSum(vector<int> &nums, int target)
-        {
-            int n=nums.size();
-            vector<vector<int>>ans;
-            // if (n < 4) return ans;
-            sort(nums.begin(), nums.end());
-            for (int i = 0; i < n; i++)
-            {
-                if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-                for (int j = i + 1; j < n-1; j++)
-                {
-                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                    int left = j + 1;
-                    int right = n - 1;
-
-                    while (left < right)
-                    {
-                        long long sum =
-                            (long long) nums[i] + nums[j] + nums[left] + nums[right];
-
-                        if (sum == target)
-                        {
-                            ans.push_back({ nums[i],
-                                nums[j],
-                                nums[left],
-                                nums[right] });
-                            left++;
-                            right--;
-
-                            while (left < right && nums[left] == nums[left - 1]) left++;
-                            while (left < right && nums[right] == nums[right + 1]) right--;
-                        }
-                        else if (sum < target)
-                        {
-                            left++;
-                        }
-                        else
-                        {
-                            right--;
-                        }
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;++i){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j > i+1 && nums[j]==nums[j-1]) continue;
+                int left=j+1;
+                int right=n-1;
+                while(left<right){
+                    long long sum = (long long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        right--;
+                        left++;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    }else if(sum<target){
+                        left++;
+                    }else{
+                        right--;
                     }
                 }
             }
-            return ans;
         }
+        return ans;
+    }
 };
