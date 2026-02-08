960a931082159f09dd630feba2c324534c397a8e
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2026-02-08 19:54:45 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.65%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;++j){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
